cc8bcdcb80671e2892565b362c9248750bf42e89
Linus Groh <mail@linusgroh.de>
2024-11-19 19:33:43 +0000

    Only use GitHub token when provided

--- src/engines/xs.js
+++ src/engines/xs.js
@@ -33,7 +33,8 @@ class XSInstaller extends Installer {
 
   static async resolveVersion(version) {
     if (version === 'latest') {
-      const body = await fetch('https://api.github.com/repos/Moddable-OpenSource/moddable-xst/releases', { headers: { 'Authorization': 'Bearer ' + process.env.GITHUB_TOKEN } })
+      const headers = process.env.GITHUB_TOKEN ? { Authorization: `Bearer ${process.env.GITHUB_TOKEN}` } : {};
+      const body = await fetch('https://api.github.com/repos/Moddable-OpenSource/moddable-xst/releases', { headers })
         .then((r) => r.json());
       return body.find((b) => !b.prerelease).tag_name.slice(1);
     }
